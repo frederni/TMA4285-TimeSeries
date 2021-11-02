0e86d6e854549a4ff88f8a49e647f1272ffd0738
frederni <frederni@stud.ntnu.no>
2021-11-02 11:32:25 +0100

    working on student with wokring fGarch

--- student_garch.R
+++ student_garch.R
@@ -1,2 +1,11 @@
 # Project part 2
-library(tseries)
+# install.packages("fGarch")
+
+library(fGarch)
+
+# Do everything in one line, maybe?
+data = read.csv('../Tidsrekker/CBLTCUSD.csv')
+stocks = cbind(data)[2] # Get the stock values only
+
+
+garchFit(data=stocks)
