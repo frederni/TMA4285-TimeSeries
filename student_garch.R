5f20467b4227e42dcd843dd2f6246bc2001733e1
frederni <frederni@stud.ntnu.no>
2021-11-02 11:55:08 +0100

    prelimiary fitting with fGARCH

--- student_garch.R
+++ student_garch.R
@@ -3,9 +3,12 @@
 
 library(fGarch)
 
-# Do everything in one line, maybe?
+# Preprocessing and data cleaning
 data = read.csv('../Tidsrekker/CBLTCUSD.csv')
-stocks = cbind(data)[2] # Get the stock values only
+stocks <- as.numeric(data[, 2]) # Get the stock values only and convert to numeric
+stocks <- na.omit(stocks) # Remove invalid stock prices
 
 
-garchFit(data=stocks)
+# Fit with GARCH(1,1) because that's the default
+fit <- garchFit(formula= ~garch(1,1), data=stocks, cond.dist="std")
+predict(fit, n.ahead = 10)
