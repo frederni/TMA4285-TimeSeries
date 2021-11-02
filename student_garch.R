c0c3f79d62d978214c023e1ea460a9b1dd241b94
frederni <frederni@stud.ntnu.no>
2021-11-02 12:09:41 +0100

    Added TODO for next time

--- student_garch.R
+++ student_garch.R
@@ -10,5 +10,10 @@ stocks <- na.omit(stocks) # Remove invalid stock prices
 
 
 # Fit with GARCH(1,1) because that's the default
-fit <- garchFit(formula= ~garch(1,1), data=stocks, cond.dist="std")
-predict(fit, n.ahead = 10)
+fit <- garchFit(formula=~garch(2,2), data=stocks, cond.dist="std")
+predict(fit, n.ahead = 10, plot=TRUE)
+
+# TODO:
+# 1) Determine why meanForecast is constant in the predict() call -- and fix
+# 2) Find optimal p, q s.t. GARCH(p, q) has the best fit
+# 3) Find out wether or not cond.dist="std" is sufficient for the student garch
