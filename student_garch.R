99cf170bfa227c3860adb159978ba71598d6d53c
frederni <frederni@stud.ntnu.no>
2021-11-09 11:50:13 +0100

    Swapped studentGarch to use lib rugarch

--- student_garch.R
+++ student_garch.R
@@ -1,19 +1,41 @@
 # Project part 2
-# install.packages("fGarch")
+install.packages("rugarch")
 
-library(fGarch)
+library(rugarch)
 
 # Preprocessing and data cleaning
 data = read.csv('../Tidsrekker/CBLTCUSD.csv')
 stocks <- as.numeric(data[, 2]) # Get the stock values only and convert to numeric
 stocks <- na.omit(stocks) # Remove invalid stock prices
 
+for(p in 0:10){
+    for(q in 0:10){
+        # TODO Check AIC/BIC for each combination
+    }
+}
 
-# Fit with GARCH(1,1) because that's the default
-fit <- garchFit(formula=~garch(2,2), data=stocks, cond.dist="std")
-predict(fit, n.ahead = 10, plot=TRUE)
+# Try using rugarch
+model1.spec <- ugarchspec(
+    variance.model = list(model = "gjrGARCH"),
+    mean.model = list(armaOrder = c(6,1), include.mean = TRUE),
+    distribution.model = "std"
+)
+model1 = ugarchfit(spec=model1.spec, data=stocks)
 
-# TODO:
-# 1) Determine why meanForecast is constant in the predict() call -- and fix
-# 2) Find optimal p, q s.t. GARCH(p, q) has the best fit
-# 3) Find out wether or not cond.dist="std" is sufficient for the student garch
+pred1 = ugarchforecast(model1, n.ahead = 20)
+
+plot(pred1) # STD distribution
+
+# ______ Using normal distribution model
+
+model2.spec <- ugarchspec(
+    variance.model = list(model = "gjrGARCH"),
+    mean.model = list(armaOrder = c(6,1), include.mean = TRUE),
+    distribution.model = "norm"
+)
+model2 = ugarchfit(spec=model2.spec, data=stocks)
+pred2 = ugarchforecast(model2, n.ahead = 20)
+plot(pred2)
+
+
+
